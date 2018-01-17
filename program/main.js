5ed0dcd25c442840dfcb4a81877317bf0fac9553
indmind <sattanationmail@gmail.com>
2018-01-17 13:21:30 +0700

    make it simple

--- program/main.js
+++ program/main.js
@@ -18,7 +18,7 @@ async function run(_code) {
     return new Promise(resolve => {
         compiler.lexString(_code, token => {
             parser.parse(token, compiled => {
-                program.run(compiled, () => resolve());
+                program.run(compiled, resolve);
             });
         });
     });
