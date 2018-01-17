e98451187381d97f23b62dfa419ef9d8e40f7849
indmind <sattanationmail@gmail.com>
2018-01-17 15:26:16 +0700

    fix npm test

--- program/test.sh
+++ program/test.sh
@@ -1,8 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-for file in ./example_testing/*.jwsl; do
-    node ./bin/jawaskrip $file
+DIRS=`ls ./example -l --time-style="long-iso" | awk '{print $8}'`
+
+STDINFILE="test.txt|angka.jws|masukan.jwsl|hujan_salju.jwsl|nsk.jwsl"
+
+for EXCLUDE in $STDINFILE; do
+    DIRS=`echo $DIRS | sed -E "s/$STDINFILE//g"`
 done
 
-echo "
-DONE!"
+for DIR in $DIRS; do
+    node ./bin/jawaskrip ./example/$DIR
+done
+
+printf "\nDONE!\n"
